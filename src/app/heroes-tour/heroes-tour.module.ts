3f4f0506b57676ebb923051c182d15937da3e395
Akihiro Yamada <u.parr917@gmail.com>
2019-05-06 22:01:38 +0900

    moduleとcomponentを作成 。

---
+++ src/app/heroes-tour/heroes-tour.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@NgModule({
+  declarations: [HeroesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroesTourModule { }
