5fe7f0b5a7339467a462401fd2d729f104cdb703
Akihiro Yamada <u.parr917@gmail.com>
2019-05-07 22:45:01 +0900

    heroのリストが表示されるように変更。

--- src/app/heroes-tour/heroes/heroes.component.ts
+++ src/app/heroes-tour/heroes/heroes.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../model/hero';
+import { HEROES } from '../mock/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
 
-  public hero: Hero = {
-    id: 1,
-    name: 'windstorm'
-  }
+  public heroes: Hero[] = HEROES; 
+  public selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
