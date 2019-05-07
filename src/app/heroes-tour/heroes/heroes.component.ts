a35c62af2a7d3437a28fd9525ea08d4077693573
Akihiro Yamada <u.parr917@gmail.com>
2019-05-07 22:08:38 +0900

    heroの表示を実装。

--- src/app/heroes-tour/heroes/heroes.component.ts
+++ src/app/heroes-tour/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../model/hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  public hero: Hero = {
+    id: 1,
+    name: 'windstorm'
+  }
   constructor() { }
 
   ngOnInit() {
